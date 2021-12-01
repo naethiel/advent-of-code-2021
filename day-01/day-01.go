87b19ce059fb211b0cbb413c82c693b0c050d7c1
Nicolas Missika <nmissika@synthesio.com>
2021-12-01 21:05:49 +0100

    day 1

--- day-01/day-01.go
+++ day-01/day-01.go
@@ -7,5 +7,34 @@ import (
 
 func main() {
 	input := utils.ReadInputInts()
-	fmt.Println(input)
+
+	var count = 0
+	var prev int64
+
+	for index, val := range input {
+		// check that we have enough elements left in the list
+		// to compute a sum of 3
+		if index > len(input)-3 {
+			continue
+		}
+
+		// compute sum of 3
+		sum := val + input[index+1] + input[index+2]
+
+		// on first iteration, skip since we have no previous value
+		if index == 0 {
+			prev = sum
+			continue
+		}
+
+		// if new sum is greater than previous one, increment
+		if sum > prev {
+			count++
+		}
+
+		// store current sum as previous
+		prev = sum
+	}
+
+	fmt.Printf("result: %d", count)
 }
